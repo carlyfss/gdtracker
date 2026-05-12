1a65678b3b695ae321785701c355f9e84284c8e1
carlyfss <carly.fs.silva@gmail.com>
2026-05-12 19:51:36 -0300

    feat: add PWA support.

--- gdtracker-web/src/main.tsx
+++ gdtracker-web/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { registerSW } from 'virtual:pwa-register'
 import App from './App.tsx'
 import { ApiUnauthorizedBridge } from './components/ApiUnauthorizedBridge'
 import { AuthProvider } from './context/AuthContext'
 import './index.css'
+
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
